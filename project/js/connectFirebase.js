07c5d7d8f11a5cfc0b3d17a6d7ef0daf27fea991
Teerasak Sathu <maxz_drum@icloud.com>
2017-03-31 04:35:53 +0700

    แก้

--- project/js/connectFirebase.js
+++ project/js/connectFirebase.js
@@ -5,13 +5,13 @@ db =firebase.database();
 var Airone = db.ref('Airone');
       Airone.on('value', function(snapshot) {
         console.log("Airone:"+snapshot.val());
-        // document.querySelector("#ledroom > input").checked = snapshot.val();
+        document.querySelector("#Airone > input").checked = snapshot.val();
 });
 
       var Airtwo = db.ref('Airtwo');
       Airtwo.on('value', function(snapshot) {
         console.log("Airtwo:"+snapshot.val());
-        // document.querySelector("#ledroom > input").checked = snapshot.val();
+       document.querySelector("#Airtwo > input").checked = snapshot.val();
 });
       var Current = db.ref('Current');
       Current.on('value', function(snapshot) {
@@ -22,22 +22,23 @@ var Airone = db.ref('Airone');
       var Light_Row_one = db.ref('Light_Row_one');
       Light_Row_one.on('value', function(snapshot) {
         console.log("Light_Row_one:"+snapshot.val());
-        // document.querySelector("#ledroom > input").checked = snapshot.val();
+       document.querySelector("#Light_Row_one > input").checked = snapshot.val();
 });
       var Light_Row_two = db.ref('Light_Row_two');
       Light_Row_two.on('value', function(snapshot) {
         console.log("Light_Row_two:"+snapshot.val());
-        // document.querySelector("#ledroom > input").checked = snapshot.val();
+         document.querySelector("#Light_Row_two > input").checked = snapshot.val();
 });
 
       var Light_Row_three = db.ref('Light_Row_three');
       Light_Row_three.on('value', function(snapshot) {
         console.log("Light_Row_three:"+snapshot.val());
-        // document.querySelector("#ledroom > input").checked = snapshot.val();
+         document.querySelector("#Light_Row_three > input").checked = snapshot.val();
 });
        var UserinRoom = db.ref('UserinRoom');
       UserinRoom.on('value', function(snapshot) {
         console.log("UserinRoom:"+snapshot.val());
+        document.getElementById('UserinRoom').innerHTML = snapshot.val();
         // document.querySelector("#ledroom > input").checked = snapshot.val();
 });
 
